12b075cb03c195e7a1b152ad339c24f199e828c6
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-22 19:27:21 +0400

    add ws

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const http = require('http');
 const fs = require('fs');
-
+const WebSocketServer = require ('ws').Server;
 
 const port = 8080;
+
+const httpServer = http.createServer((req, res) => {
+  switch (req.url) {
+ case '/':
+   fs.readFile('public/index.html',
+   'utf-8',
+   (_, data) => res.end(data));
+   break;
+   case '/bundle.js':
+    fs.readFile('public/bundle.js',
+		'utf-8',
+		(_, data) => res.end(data));
+    break;
+  default: res.end();}
+};
+
+httpServer.listen(port, () =>
+		  console.log(`Server started on port:${port}, check localhost:${port}`)
+);
