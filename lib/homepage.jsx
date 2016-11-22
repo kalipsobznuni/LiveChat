221efd826c14d7a4527a24f10674c890ab3066c4
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-22 19:08:22 +0400

    add starter code

---
+++ lib/homepage.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Clicker extends React.Component {
+
+  }
+
+ReactDOM.render(<Clicker />,
+		document.getElementById('react-container'));
